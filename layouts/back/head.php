f5d3ee32636e8bd8f10b29290d3aaa5f9b4e9ba3
Arthaud Proust <arthaudp33@gmail.com>
2022-02-16 10:12:50 +0100

    add header to layout

--- layouts/back/head.php
+++ layouts/back/head.php
@@ -5,14 +5,17 @@ require_once __DIR__ . '../../../util/index.php';
 
 <!DOCTYPE html>
 <html lang="fr-FR">
-<head>
-    <meta charset="utf-8" />
-    <title><?= isset($pageTitle)?$pageTitle:'Title' ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <head>
+        <meta charset="utf-8" />
+        <title><?= isset($pageTitle)?$pageTitle:'Title' ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link href="<?= webSitePath() ?>/assets/css/var.css" rel="stylesheet" type="text/css" />
-    <link href="<?= webSitePath() ?>/assets/css/master.css" rel="stylesheet" type="text/css" />
-    <link href="<?= webSitePath() ?>/assets/css/admin.css" rel="stylesheet" type="text/css" />
-    <!-- <link href="<?= webSitePath() ?>/assets/css/style.css" rel="stylesheet" type="text/css" /> -->
-</head>
-<body>
+        <link href="<?= webSitePath() ?>/assets/css/var.css" rel="stylesheet" type="text/css" />
+        <link href="<?= webSitePath() ?>/assets/css/master.css" rel="stylesheet" type="text/css" />
+        <link href="<?= webSitePath() ?>/assets/css/admin.css" rel="stylesheet" type="text/css" />
+        <!-- <link href="<?= webSitePath() ?>/assets/css/style.css" rel="stylesheet" type="text/css" /> -->
+    </head>
+    <body>
+        <?php 
+            require_once __DIR__ . './../general/header.php';
+        ?>

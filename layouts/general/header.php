7347c31d01e3b68fa5d43fd2927c4915374febf2
Nyxmajson <nago.jason@gmail.com>
2022-02-16 10:29:31 +0100

    Modification layout/general/header.php

--- layouts/general/header.php
+++ layouts/general/header.php
@@ -1,3 +1,10 @@
 <header>
-    Header
+    <nav>
+        <ul>
+            <li class="interactive"><a href=""> <img src="\assets\img\logo.png" alt="logo du site du blog"></a></li>
+            <li class="interactive"><a href="">Connexion</a></li>
+            <li class="interactive"><a href="">Inscription</a></li>
+            <li class="interactive"><a href="">Se déconnecter</a></li>  
+        </ul>  
+    </nav>
 </header>

9b4c8e1d268b450b93bc05b658e1aa339a016bf7
Quentin Wegera <quentin.wegera@mmibordeaux.com>
2022-02-17 11:37:31 +0100

    updateMembre Quentin

--- back/membre/updateMembre.php
+++ back/membre/updateMembre.php
@@ -1,78 +1,78 @@
 <?php
-////////////////////////////////////////////////////////////
-//
-//  CRUD MEMBRE (PDO) - Modifié : 4 Juillet 2021
-//
-//  Script  : updateMembre.php  -  (ETUD)  BLOGART22
-//
-////////////////////////////////////////////////////////////
-
-// Mode DEV
-require_once __DIR__ . '/../../util/utilErrOn.php';
-
-// controle des saisies du formulaire
-require_once __DIR__ . '/../../util/ctrlSaisies.php';
-
-// Mise en forme date
-require_once __DIR__ . '/../../util/dateChangeFormat.php';
+// Insertion des fonctions utilitaires
+require_once __DIR__ . '/../../util/index.php';
 
 // Insertion classe Membre
+require_once __DIR__ . '/../../CLASS_CRUD/membre.class.php'; 
 
 // Instanciation de la classe Membre
+$monMembre = new MEMBRE(); 
 
+//Insertion de la classe Statut
+require_once __DIR__ . '/../../CLASS_CRUD/statut.class.php';
+
+//Instanciation de le classe Statut
+$monStatut = new STATUT();
 
 // Gestion des erreurs de saisie
 $erreur = false;
-// Init msg
-
 
 // Gestion du $_SERVER["REQUEST_METHOD"] => En POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 
+    if(isset($_POST['Submit'])){
+        $Submit = $_POST['Submit'];
+    } else {
+        $Submit = "";
+    }
 
-    // controle des saisies du formulaire
+    $validator = Validator::make([
+        ValidationRule::required('id'),
+        ValidationRule::required('prenomMemb'),
+        ValidationRule::required('nomMemb'),
+        ValidationRule::required('pseudoMemb'),
+        ValidationRule::required('passMemb'),
+        ValidationRule::required('eMailMemb'),
+        ValidationRule::required('dtCreaMemb'),
+        ValidationRule::required('accordMemb'),
+        ValidationRule::required('idStat')
+    ])->bindValues($_POST);
 
-    // modification effective du membre
+    if($validator->success()) {
+        // Saisies valides
+        $erreur = false;
+
+        $numMemb = $validator->verifiedField('id');
+        $prenomMemb = $validator->verifiedField('prenomMemb');
+        $nomMemb = $validator->verifiedField('nomMemb');
+        $pseudoMemb = $validator->verifiedField('pseudoMemb');
+        $passMemb = $validator->verifiedField('passMemb');
+        $eMailMemb = $validator->verifiedField('eMailMemb');
+        $dtCreaMemb = $validator->verifiedField('dtCreaMemb');
+        $accordMemb = $validator->verifiedField('accordMemb');
+        $idStat = $validator->verifiedField('idStat');
+        $monMembre->update($numMemb, $prenomMemb, $nomMemb, $pseudoMemb, $passMemb, $eMailMemb, $dtCreaMemb, $accordMemb, $idStat);
 
 
-
-    // Gestion des erreurs => msg si saisies ko
-
-            // CTRL saisies
-            // VALIDITÉ MAIL
-
-            // MAIL IDENTIQUE
-            // TEST MODIF PASS
-
-
-
-
-
+        header("Location: ./angle.php");
+    } else {
+        // Saisies invalides
+        $erreur = true;
+        $errSaisies =  "Erreur, la saisie est obligatoire !";
+    }   // End of else erreur saisies
 }   // Fin if ($_SERVER["REQUEST_METHOD"] === "POST")
+
+
 // Init variables form
 include __DIR__ . '/initMembre.php';
-?>
-<!DOCTYPE html>
-<html lang="fr-FR">
-<head>
-    <meta charset="utf-8" />
-    <title>Admin - CRUD Membre</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
 
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-        .error {
-            padding: 2px;
-            border: solid 0px black;
-            color: red;
-            font-style: italic;
-            border-radius: 5px;
-        }
-    </style>
-    <script>
+$pageTitle = "Modifier un Membre";
+$pageNav = ['Home:/index1.php', 'Gestion Membre:./membre.php', $pageTitle];
+include __DIR__ . '/../../layouts/back/head.php';
+?>
+
+<script>
         // Affichage pass
         function myFunction(myInputPass) {
             var x = document.getElementById(myInputPass);
@@ -82,50 +82,59 @@ include __DIR__ . '/initMembre.php';
               x.type = "password";
             }
         }
-    </script>
-</head>
-<body>
-    <h1>BLOGART22 Admin - CRUD Membre</h1>
-    <h2>Modification d'un membre</h2>
+</script>
+
 <?php
     // Modif : récup id à modifier
     // id passé en GET
 
-
-
-
-
-
+    if(!isset($_GET['id'])) {
+        header("Location: ./membre.php");
+        die();
+    }
+    $membre = $monMembre->get_1Membre($_GET['id']);
+    $prenomMemb = $membre['prenomMemb'];
+    $nomMemb = $membre['nomMemb'];
+    $pseudoMemb = $membre['pseudoMemb'];
+    $passMemb = $membre['passMemb'];
+    $eMailMemb = $membre['eMailMemb'];
+    $dtCreaMemb = $membre['dtCreaMemb'];
+    $accordMemb = $membre['accordMemb'];
+    $idStat = $membre['idStat'];
 
 ?>
-    <form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" accept-charset="UTF-8">
+    <form 
+        class="admin-form"
+        method="POST" 
+        action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>?id=<?=$_GET['id'] ?>" 
+        enctype="multipart/form-data" 
+        accept-charset="UTF-8"
+    >
+        <input type="hidden" id="id" name="id" value="<?=$_GET['id'] ?>" />
 
-      <fieldset>
-        <legend class="legend1">Formulaire Membre...</legend>
-
-        <input type="hidden" id="id" name="id" value="<?= isset($_GET['id']) ? $_GET['id'] : '' ?>" />
-
-        <div class="control-group">
-            <label class="control-label" for="prenomMemb"><b>Prénom<span class="error">(*)</span> :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="prenomMemb" id="prenomMemb" size="80" maxlength="80" value="<?= $prenomMemb; ?>" autocomplete="on" autofocus="autofocus" />
+        <div class="field">
+            <label for="prenomMemb">Prénom</label>
+            <input name="prenomMemb" id="prenomMemb" size="80" maxlength="80" value="<?= $prenomMemb; ?>" />
         </div>
 
-        <br>
-        <div class="control-group">
-            <label class="control-label" for="nomMemb"><b>Nom<span class="error">(*)</span> :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="nomMemb" id="nomMemb" size="80" maxlength="80" value="<?= $nomMemb; ?>" autocomplete="on" />
+        <div class="field">
+            <label for="nomMemb">Nom</label>
+            <input name="nomMemb" id="nomMemb" size="80" maxlength="80" value="<?= $nomMemb; ?>" />
         </div>
 
-        <br>
-        <div class="control-group">
-            <label class="control-label" for="pseudoMemb"><b>Pseudonyme :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="pseudoMemb" id="pseudoMemb" size="80" maxlength="80" value="<?= $pseudoMemb; ?>" disabled />
+        <div class="field">
+            <label for="pseudoMemb">Pseudo</label>
+            <input name="pseudoMemb" id="pseudoMemb" size="80" maxlength="80" value="<?= $pseudoMemb; ?>" disabled/>
         </div>
 
-        <br>
-        <div class="control-group">
+        <!--<div class="field">
+            <label for="pass1Memb">Mot de passe</label>
+            <input name="passMemb" id="passMemb" size="80" maxlength="80" value="<?= $passMemb; ?>" />
+        </div>-->
+
+        <div class="field">
             <label class="control-label" for="pass1Memb"><b>Mot passe<span class="error">(*)</span> :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="password" name="pass1Memb" id="myInput1" size="80" maxlength="80" value="<?= $pass1Memb; ?>" autocomplete="on" />
+            <input type="password" name="pass1Memb" id="myInput1" size="80" maxlength="80" value="<?= $passMemb; ?>" autocomplete="on" />
             <br>
             <input type="checkbox" onclick="myFunction('myInput1')">
             &nbsp;&nbsp;
@@ -133,99 +142,47 @@ include __DIR__ . '/initMembre.php';
         </div>
 
         <br>
-        <div class="control-group">
+        <div class="field">
             <label class="control-label" for="pass2Memb"><b>Confirmez le mot passe<span class="error">(*)</span> :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="password" name="pass2Memb" id="myInput2" size="80" maxlength="80" value="<?= $pass2Memb; ?>" autocomplete="on" />
+            <input type="password" name="pass2Memb" id="myInput2" size="80" maxlength="80" value="<?= $passMemb; ?>" autocomplete="on" />
             <br>
             <input type="checkbox" onclick="myFunction('myInput2')">
             &nbsp;&nbsp;
             <label><i>Afficher mot de passe</i></label>
         </div>
         <small class="error">*Champ obligatoire si nouveau passe</small><br>
-        <br>
-        <div class="control-group">
-            <label class="control-label" for="eMail1Memb"><b>eMail<span class="error">(*)</span> :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="email" name="eMail1Memb" id="eMail1Memb" size="80" maxlength="80" value="<?= $eMail1Memb; ?>" autocomplete="on" />
+        
+        <div class="field">
+            <label for="eMailMemb">EMail</label>
+            <input name="eMailMemb" id="eMailMemb" size="80" maxlength="80" value="<?= $eMailMemb; ?>" />
         </div>
 
-        <br>
-        <div class="control-group">
-            <label class="control-label" for="eMail2Memb"><b>Confirmez l'eMail<span class="error">(*)</span> :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="email" name="eMail2Memb" id="eMail2Memb" size="80" maxlength="80" value="<?= $eMail2Memb; ?>" autocomplete="on" />
+        <div class="field">
+            <label for="dtCreaMemb">Date de création</label>
+            <input name="dtCreaMemb" id="dtCreaMemb" size="80" maxlength="80" value="<?= $dtCreaMemb; ?>" />
         </div>
-        <small class="error">*Champ obligatoire si nouveau eMail</small><br>
 
-        <br>
-        <div class="control-group">
-            <label class="control-label" for="accordMemb"><b>J'ai accepté que mes données soient conservées :</b></label>
-            <div class="controls">
-               <fieldset>
-                  <input type="radio" name="accordMemb"
-                  <? if($accordMemb == 1) echo 'checked="checked"'; ?>
-                  value="on" disabled />&nbsp;&nbsp;Oui&nbsp;&nbsp;&nbsp;&nbsp;
-                  <input type="radio" name="accordMemb"
-                  <? if($accordMemb == 0) echo 'checked="checked"'; ?>
-                  value="off" disabled />&nbsp;&nbsp;Non
-               </fieldset>
-            </div>
+        <div class="field">
+            <label for="accordMemb">Accord du membre au RGPD</label>
+            <input name="accordMemb" id="accordMemb" size="80" maxlength="80" value="<?= $accordMemb; ?>" />
         </div>
-        <br><br>
 
-<!-- --------------------------------------------------------------- -->
-<!-- --------------------------------------------------------------- -->
-    <!-- FK : Statut -->
-<!-- --------------------------------------------------------------- -->
-    <!-- Listbox statut -->
-        <div class="control-group">
-            <label class="control-label" for="LibTypStat"><b>Statut :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-                <input type="hidden" id="idStat" name="idStat" value="<?= isset($_GET['idStat']) ? $_GET['idStat'] : '' ?>" />
-
-                <input type="text" name="idStat" id="idStat" size="5" maxlength="5" value="<?= $idStat; ?>" autocomplete="on" />
-
-                <!-- Listbox statut => 2ème temps -->
-
+        <div class="field">
+            <label for="idStat">Quel statut :</label>
+            <select name="idStat" id="idStat">
+            <?php 
+                $allStatuts = $monStatut->get_AllStatuts();                    
+                foreach($allStatuts as $statut) { 
+            ?>
+                <option <?=$statut['idStat']==$idStat?'selected':'' ?> value="<?= $statut['idStat'] ?>" ><?=$statut['libStat'] ?></option>
+            <?php } ?>
+            </select>
         </div>
-    <!-- FIN Listbox statut -->
-<!-- --------------------------------------------------------------- -->
-    <!-- FK : Statut -->
-<!-- --------------------------------------------------------------- -->
-<!-- --------------------------------------------------------------- -->
-        <br>
-        <div class="control-group">
-            <label class="control-label" for="dtCreaMemb"><b>Date création :&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="dtCreaMemb" id="dtCreaMemb" value="<?= $dtCreaMemb; ?>" disabled />
-        </div>
-        <small>(Pour mémoire)</small><br>
 
-        <div class="control-group">
-            <div class="error">
-<?php
-            if ($erreur) {
-                echo ($errSaisies);
-            }
-            else {
-                $errSaisies = "";
-                echo ($errSaisies);
-            }
-?>
-            </div>
+        <div class="controls">
+            <a class="btn btn-lg btn-text" href="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>?id=<?=$_GET['id'] ?>">Réinitialiser</a>
+            <a class="btn btn-lg btn-secondary" href="./membre.php">Annuler</a>
+            <input class="btn btn-lg" type="submit" value="Valider" />
         </div>
-        <div class="control-group">
-            <div class="controls">
-                <br><br>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" value="Initialiser" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" value="Valider" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                <br>
-            </div>
-        </div>
-      </fieldset>
     </form>
-<?php
-require_once __DIR__ . '/footerMembre.php';
-
-require_once __DIR__ . '/footer.php';
-?>
-</body>
-</html>
+<?php require_once __DIR__ . '/../../layouts/back/foot.php'; ?>

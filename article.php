9f3fe055a9eaed3715b1b4cb60fe9e4a6fb21df7
Maxime <maxime.lust@etudiant.univ-lr.fr>
2022-02-17 17:01:42 +0100

    article de la page article de l'expo

---
+++ article.php
@@ -0,0 +1,78 @@
+<?php
+require_once __DIR__ . '/util/index.php';
+
+
+
+$pageTitle = "Panel admin";
+$pageNav = ['Home'];
+require_once __DIR__ . '/layouts/front/head.php';
+?>
+    <div class="head-bg"> <!-- <img src="" alt="Oeuvre de David Selor de son personnage Mimil, servant d'arrière plan"> -->
+        <div class="head-title-h1">
+
+        </div>
+        <div class="head-title-h2">
+
+        </div>
+        <div class="head-text">
+
+        </div>
+    </div>
+    <div class="discover">
+        <div class="discover-title">
+            <h2>Decouvrez-en plus sur ce personnage que vous ne connaissez que tres peu… <span class="orange">Pour l’instant</span> !</h2>
+        </div>
+    </div>
+    <div class="container articles">
+        <div class="david-selor">
+            <div class="ds-text">
+                <p>Né à Cognac en 1988, David Selor est un street artist bordelais. A l’âge de 10 ans, le peintre à l’aérosol Seron Monbaton est venu graffer dans son école. C’est une révélation. Depuis ce jour, il n’a jamais oublié les graffitis et a toujours gardé en tête le milieu du street art. Pour pouvoir signer ses créations, il choisit le pseudonyme “Selor”. Celui-ci est une simple suite de lettres dont il apprécie tout particulièrement l’enchaînement ainsi que la graphie. Peu avant de se consacrer entièrement à sa passion, il effectue en 2013 un service civique à Lisbonne, dans un centre d’accueil pour personnes autistes. Cette expérience est un véritable tournant dans sa vie. C’est en effet là qu’il crée son personnage que l’on connaît aujourd’hui : le Mimil. Par la suite, il développera ses œuvres sur les murs de la ville de Bordeaux ainsi que sur des toiles à l'acrylique. David Selor a principalement peint dans la ville de Bordeaux ainsi que dans de nombreuses autres villes telles que Nantes, Marseille, Lyon et bien sûr Paris. Notre street artist traverse même les frontières en peignant les métropoles de Bruxelles, Londres, Athènes, Rome ou encore Porto.</p>
+            </div>
+            <div class="ds-title">
+                <h2><span class="purple">Qui est David Selor ?</span></h2>
+            </div>
+        </div>
+        <div class="mimil">
+            <div class="mimil-title">
+                <h2><span class="green">Vous l’avez sûrement déjà vu. Retour sur Mimil, ce personnage qui nous replonge en enfance.</span></h2>
+            </div>
+            <div class="mimil-text">
+                <p>Imaginant l’humain dans sa forme la plus instinctive, ce personnage chimérique se balade dans les plus grandes villes d’Europe. Mi-humain mi-animal, il apparaît plus d’une centaine de fois au cours de ses tribulations, ne serait-ce que dans la métropole bordelaise. Les yeux fermés, le corps jaune pour se démarquer, une marinière bleue et blanche pour rappeler ses origines françaises, il est reconnaissable entre mille. Malgré un graphisme accrocheur, c’est bien le message qui est mis en avant. En effet, Mimil s’exprime avec un ton léger, humoristique, et s’inspire des comportements de l’être humain tout en poésie. Ainsi, ses messages, quand ils ont un sens, sont laissés ouverts à l'interprétation, pour ceux qui prennent le temps de les lire et d’y réfléchir. Il aborde de nombreux sujets, mais revendique principalement sa liberté d’aller peindre sur les murs, dans le respect d’autrui. De véritables safaris sont organisés dans le but de capturer le plus d’apparitions de ce personnage, que ce soit sur les parpaings de Bordeaux, ou dans d’autres villes. Les Mimils, c’est la signature de David Selor, c’est la représentation de l’être humain, et c’est à vous d’en décoder les secrets.</p>
+            </div>
+        </div>
+        <div class="expo">
+            <div class="expo-title">
+                <h2><span class="orange">Une exposition à ne pas manquer !</span></h2>
+            </div>
+            <div class="expo-text">
+                <p>En ce début d’année, David Selor et son Mimil posent leurs valises à l'institut Bernard Magrez, centre contemporain de Bordeaux convoité par l’artiste depuis quelques années. Avec Fragile, il marque un renouveau, une remise en question qui se devine ne serait-ce que par le format de l’exposition. C’est en effet pas moins de 20 toiles qui sont exposées. Lui qui a tendance à réaliser ses Mimils en une quinzaine de minutes sur les murs, ici, il a parfois fallu jusqu’à plusieurs semaines pour les finaliser. Fragile, comme l’éphémérité des œuvres dans la rue, qui peuvent être effacées, détruites à cause de la nature de leur support. David Selor s’exprime sur des sujets variés tels que la solitude à plusieurs, de la beauté dans l’imperfection et la dégradation, de la dualité de l’amour et la de haine, ou encore de la rue et de la toile. Tout comme avec les Mimils dans la rue, il laisse le soin aux visiteurs de tirer leur propre interprétation, de faire écho avec leur expérience personnelle qui est unique pour chacun. Que verrez-vous, vous ? Peut-être même que vous remarquerez une évolution du Mimil, mais cela, c’est si vous acceptez de la voir.</p>
+            </div>
+            <div class="conclusion">
+
+            </div>
+        </div>
+        <div class="articles-end">
+            <div class="auteur">
+
+            </div>
+            <div class="likes">
+
+            </div>
+        </div>
+        <div class="comments">
+
+        </div>
+        <div class="add-comments">
+            <div class="text-comments">
+
+            </div>
+            <div class="bloc-comments">
+
+            </div>
+            <div class="btn-comments">
+
+            </div>
+        </div>
+    </div>
+
+<?php require_once __DIR__ . '/layouts/front/foot.php';?>
---
+++ layouts/front/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="utf-8" />
+        <title><?= isset($pageTitle)?$pageTitle:'Title' ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link href="<?= webSitePath() ?>/assets/css/var.css" rel="stylesheet" type="text/css" />
+        <link href="<?= webSitePath() ?>/assets/css/master.css" rel="stylesheet" type="text/css" />
+        <!-- <link href="<?= webSitePath() ?>/assets/css/style.css" rel="stylesheet" type="text/css" /> -->
+    </head>
+    <body>
+        <?php require_once __DIR__ . './../general/header.php'; ?>
+        <main class="container-fluid">

16eb19950f4600e168cc1a4e836d3fee9fd36869
Quentin Wegera <quentin.wegera@mmibordeaux.com>
2022-02-18 14:06:30 +0100

    debut connexion utilisateur

--- connexion.php
+++ connexion.php
@@ -7,18 +7,60 @@ $pageTitle = "$submitBtn: $pageCrud";
 $pageNav = ['Home:/index1.php', 'Gestion des '.$pageCrud.'s:'.$pagePrecedent, $pageTitle];
 
 require_once __DIR__ . '/util/index.php';
+require_once __DIR__ . '/CONNECT/database.php';
+require_once __DIR__ . '/layouts/front/head.php';
 
 $pageTitle = "Panel user";
 $pageNav = ['Connexion'];
-require_once __DIR__ . '/layouts/front/head.php';
+
+global $db;
+
+
+// $query = 'SELECT * FROM MEMBRE WHERE numMemb = ?;';
+// $result = $db->prepare($query);
+// $result->execute([$_SESSION['numMemb']]);
+// $rowU = $result->fetch();
+// if($rowU){
+//     $numMemb = $rowU['numMemb'];
+//     $your_name = $rowU['your_name'];
+// }
+
+
+
+$pseudoMemb = ctrlSaisies($_POST['pseudoMemb']);
+$passMemb = ctrlSaisies($_POST['passMemb']);
+
+$query = "SELECT * FROM MEMBRE WHERE pseudoMemb = ? AND passMemb = ?";
+$result = $db->prepare($query);
+$result->execute([$pseudoMemb, $passMemb]);
+$rowCount = $result->rowCount();
+
+if($rowCount < 1){
+	$_SESSION['message'] = "Erreur de Login. Veuillez réessayer.";
+	// header('location: connexion.php');
+}else{
+	$row = $result->fetch();
+	$_SESSION['userid'] = $row['userid'];
+	header('location: index1.php');
+}
 ?>
 
+
+<h1>Connexion</h1>
 <form method="POST" action="index1.php">
-    <label>Pseudo</label>
-	<input type="text" name="username">
-    <label> Mot de passe </label>
-	<input type="password" name="password"> <br/><br/>
+
+    <div class="field">
+        <label>Pseudo</label>
+        <input type="text" name="pseudoMemb">
+    </div>
+    
+    <div class="field">
+        <label> Mot de passe </label>
+        <input type="passMemb" name="passMemb"> <br/><br/>
+    </div>
+
     <input class="btn btn-lg" title="Se connecter" type="submit" value="Connexion" />
+
 </form>
 
 <?php require_once __DIR__ . '/layouts/front/foot.php';?>

e661374a0f577ccec62175a4243955a49cc6de05
Maxime <maxime.lust@etudiant.univ-lr.fr>
2022-03-02 18:16:26 +0100

    mentions légales

--- mentions-legales.php
+++ mentions-legales.php
@@ -2,6 +2,20 @@
 require_once __DIR__ . '/layouts/front/head.php';
 ?>
 <div class="container">
+    <br>
     <h2>Mentions légales</h2>
+    <br>
+    <br>
+    <h4>Propriétaire du site</h4>
+    <p>L’édition et la direction de la publication du site sont assurées par notre représentant légal Simone Beauvoir, domiciliée 1 Rue Jacques Ellul, 33800 Bordeaux, dont le numéro de téléphone est 0798524112, et l'adresse e-mail:
+- studiobeauvoir@mmibordeaux.com</p>
+    <br>
+    <br>
+    <h4>Hébergeur</h4>
+    <p>Le site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découler d’une nécessité de maintenance.</p>
+    <br>
+    <br>
+    <h4>Cookies</h4>
+    <p>Des cookies de session, évitant aux utilisateurs de renseigner plusieurs fois les mêmes informations telles que les identifiants de connexion.</p>
 </div>
 <?php require_once __DIR__ . '/layouts/front/foot.php'; ?>

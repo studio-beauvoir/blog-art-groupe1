784329b908b2523dd95c5e53bcfa439c878d20aa
Nyxmajson <nago.jason@gmail.com>
2022-03-02 18:19:06 +0100

    j'ai des trucs de ouf

--- cgu.php
+++ cgu.php
@@ -1,16 +1,20 @@
 <?php
 require_once __DIR__ . '/layouts/front/head.php';
 ?>
-<div class="container patate">
+<div class="container ">
+    <br>
+    <br>
     <h2>Conditions générales d'utilisation</h2>
     <p> 
     Nous sommes responsables de ce qui est publié et nous avons le choix de la ligne éditoriale, le droit de propriété intellectuelle s’applique. Plus précisément, l’édition et la direction de la publication du site sont assurées par notre représentant légal Simone Beauvoir, domiciliée 1 Rue Jacques Ellul, 33800 Bordeaux, dont l'adresse mail est :<br>
     - studiobeauvoir@mmibordeaux.com <br>
     Pour certifier nos droits de propriété intellectuelle, chaque article et commentaires seront daté et horodaté.
     <br>
+    <br>
     Dans notre cas, il se peut que nous possédions des animateurs à long terme sur le blog. Ce qui les place en tant que responsables, ils devront par conséquent modérer.
     De ce fait, on impose une politique de confidentialité puisqu’on récolte les données de nos utilisateurs européens / bordelais. Les éditeurs du site récolteront vos données suite à votre inscription sur le blog. Chacune des données sera traitée de manière anonyme et uniquement accessible pour vous et l’éditeur, à l’exception de votre pseudonyme conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Il sera accessible à tout le monde pour bâtir votre identité sur notre blog. Tout citoyen a le droit au respect de sa personne. Les cookies seront strictement nécessaires au bon fonctionnement du site, par conséquent ils seront obligatoires dans les respects de confidentialité de nos utilisateurs. Ce seront des cookies de session, évitant aux utilisateurs de renseigner plusieurs fois les mêmes informations telles que les identifiants de connexion. <br>
     L’utilisateur peut attaquer quelqu’un qui porterait atteinte à sa personnalité et son honneur.
+    <br>
     <br>
     Le site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découler d’une nécessité de maintenance. En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'utilisateur exerce ce droit. En cas de modification, interruption ou suspension du site, l'éditeur ne saurait être tenu responsable.<br>
     En cas de litige, toute personne pourra récupérer les données qu’elle a fournies sous une forme aisément réutilisable. En dessous de 16 ans, les utilisateurs devront se munir d’un consentement parental. 

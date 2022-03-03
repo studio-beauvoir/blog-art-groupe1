92c718fe7affde56a79a039cd96a5323359a29d0
Arthaud Proust <arthaudp33@gmail.com>
2022-03-03 12:16:30 +0100

    fix prod

--- CONNECT/configProd.php
+++ CONNECT/configProd.php
@@ -8,7 +8,7 @@ $nomBD = "db_mmi_01";
 $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
 
 // nom utilisateur de connexion à la BDD
-$userBD = 'etummiuser_db_XX';         // Votre login
+$userBD = 'etummiuser_db_01';         // Votre login
 // mot de passe de connexion à la BDD
 // $passBD = 'root';         // Pass WAMP MAC
 $passBD = 'mmi-etu';         // Pass WAMP WINDOW

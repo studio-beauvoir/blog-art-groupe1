837d2fe255c586074882a11d4c910243d4b201cb
Arthaud Proust <arthaudp33@gmail.com>
2022-03-03 15:42:43 +0100

    add auth user

--- admin/inscription.php
+++ admin/inscription.php
@@ -1,46 +1,44 @@
 <?php
 require_once __DIR__ . '/../util/index.php';
 
-require_once __DIR__ . '/../class_crud/membre.class.php';
-$monMembre = new membre();
+require_once __DIR__ . '/../class_crud/user.class.php';
+$monUser = new user();
 
 $validator = Validator::make();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $validator->addRules([
-        ValidationRule::required('prenomMemb'),
-        ValidationRule::required('nomMemb'),
-        ValidationRule::required('pseudoMemb')->pseudo()->unique('membre')->customError('shouldBeUnique', 'Ce pseudo est déjà pris'),
-        ValidationRule::required('passMemb')->password(),
-        ValidationRule::required('passMemb_confirm')->password()->equalTo('passMemb'),
-        ValidationRule::required('eMailMemb')->email()->unique('membre')->customError('shouldBeUnique', 'Cet email est déjà pris'),
-        ValidationRule::required('eMailMemb_confirm')->email()->equalTo('eMailMemb'),
-        ValidationRule::required('accordMemb')->equalToValue('on')->customError('shouldBeEqualToValue', 'Vous devez accepter les conditions d\'utilisation'),
+        ValidationRule::required('prenomUser'),
+        ValidationRule::required('nomUser'),
+        ValidationRule::required('pseudoUser')->pseudo()->unique('user')->customError('shouldBeUnique', 'Ce pseudo est déjà pris'),
+        ValidationRule::required('passUser')->password(),
+        ValidationRule::required('passUser_confirm')->password()->equalTo('passUser'),
+        ValidationRule::required('eMailUser')->email()->unique('user')->customError('shouldBeUnique', 'Cet email est déjà pris'),
+        ValidationRule::required('eMailUser_confirm')->email()->equalTo('eMailUser'),
     ])->bindValues($_POST);
 
     if($validator->success()) {
-        $prenomMemb = $validator->verifiedField('prenomMemb');
-        $nomMemb = $validator->verifiedField('nomMemb');
-        $pseudoMemb = $validator->verifiedField('pseudoMemb');
+        $prenomUser = $validator->verifiedField('prenomUser');
+        $nomUser = $validator->verifiedField('nomUser');
+        $pseudoUser = $validator->verifiedField('pseudoUser');
 
-        $passMemb = $validator->verifiedField('passMemb', false);
+        $passUser = $validator->verifiedField('passUser', false);
         // hashage du mot de passe
-        $passMemb = password_hash($passMemb, PASSWORD_BCRYPT);
+        $passUserHash = password_hash($passUser, PASSWORD_BCRYPT);
 
-        $eMailMemb = $validator->verifiedField('eMailMemb');
+        $eMailUser = $validator->verifiedField('eMailUser');
 
         date_default_timezone_set("Europe/Paris");
-        $dtCreaMemb = date("Y-m-d H:i:s");
+        $dtCreaUser = date("Y-m-d H:i:s");
         
-        $accordMemb = $validator->verifiedField('accordMemb')=="on";
 		
-        $idStat = 3; // Membre niveau 1
+        $idStat = 3; // User niveau 1
 
         
-        $monMembre->create($prenomMemb, $nomMemb, $pseudoMemb, $passMemb, $eMailMemb, $dtCreaMemb, $accordMemb, $idStat);
+        $monUser->create($pseudoUser, $nomUser, $prenomUser,  $eMailUser, $passUserHash, $idStat);
         
-        $monMembre->login($pseudoMemb, $passMemb);
+        $monUser->login($pseudoUser, $passUser);
         die();
     } else {
         // Saisies invalides
@@ -50,9 +48,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 } 
 
-$pageTitle = "Panel user";
-$pageNav = ['Inscription'];
-require_once __DIR__ . '/layouts/front/head.php';
+$pageTitle = "Accès admin - inscription";
+require_once __DIR__ . '/../layouts/front/head.php';
 ?>
 
 <script>
@@ -70,7 +67,8 @@ require_once __DIR__ . '/layouts/front/head.php';
 
 
 <div class="container">
-    <h1>S'inscrire</h1>
+    <h2>Créer un compte</h2>
+    <h3>Accès admin</h3>
     <?=$validator->echoErrors()?>
     <form
         class="user-form"
@@ -81,34 +79,34 @@ require_once __DIR__ . '/layouts/front/head.php';
     >
 
         <div class="field">
-            <label for="nomMemb">Nom</label>
-            <input value="<?=$validator->oldField('nomMemb')?>" name="nomMemb" id="nomMemb" placeholder="" size="100" maxlength="100" />
+            <label for="nomUser">Nom</label>
+            <input value="<?=$validator->oldField('nomUser')?>" name="nomUser" id="nomUser" placeholder="" size="100" maxlength="100" />
         </div>
 
         <div class="field">
-            <label for="prenomMemb">Prénom</label>
-            <input value="<?=$validator->oldField('prenomMemb')?>" name="prenomMemb" id="prenomMemb" placeholder="" size="100" maxlength="100" />
+            <label for="prenomUser">Prénom</label>
+            <input value="<?=$validator->oldField('prenomUser')?>" name="prenomUser" id="prenomUser" placeholder="" size="100" maxlength="100" />
         </div>
 
         <div class="field">
-            <label for="prenomMemb">Pseudo</label>
-            <input value="<?=$validator->oldField('pseudoMemb')?>" name="pseudoMemb" id="pseudoMemb" placeholder="" size="100" maxlength="100" />
+            <label for="prenomUser">Pseudo</label>
+            <input value="<?=$validator->oldField('pseudoUser')?>" name="pseudoUser" id="pseudoUser" placeholder="" size="100" maxlength="100" />
         </div>
 
         <div class="field">
-            <label for="eMailMemb">Email</label>
-            <input value="<?=$validator->oldField('eMailMemb')?>" name="eMailMemb" id="eMailMemb" size="80" maxlength="80" />
+            <label for="eMailUser">Email</label>
+            <input value="<?=$validator->oldField('eMailUser')?>" name="eMailUser" id="eMailUser" size="80" maxlength="80" />
         </div>
 
         <div class="field">
-            <label for="eMailMemb_confirm">Confirmez l'eMail</label>
-            <input value="<?=$validator->oldField('eMailMemb_confirm')?>" name="eMailMemb_confirm" id="eMailMemb_confirm" size="80" maxlength="80" />
+            <label for="eMailUser_confirm">Confirmez l'eMail</label>
+            <input value="<?=$validator->oldField('eMailUser_confirm')?>" name="eMailUser_confirm" id="eMailUser_confirm" size="80" maxlength="80" />
         </div>
 
         <div class="field">
             <label>Mot de passe</label>
-            <input value="<?=$validator->oldField('passMemb')?>" type="password" name="passMemb" id="passMemb">
-            <label><input type="checkbox" onclick="myFunction('passMemb')"><i>Afficher le mot de passe</i></label>
+            <input value="<?=$validator->oldField('passUser')?>" type="password" name="passUser" id="passUser">
+            <label><input type="checkbox" onclick="myFunction('passUser')"><i>Afficher le mot de passe</i></label>
             <p>
                 Le mot de passe doit comporter entre 6 et 15 caractères, 
                 <br/>et au moins une lettre, un chiffre et un caractère spécial parmi &@#$%_-.?!
@@ -117,16 +115,8 @@ require_once __DIR__ . '/layouts/front/head.php';
 
         <div class="field">
             <label>Confirmer le mot de passe</label>
-            <input value="<?=$validator->oldField('passMemb_confirm')?>" type="password" name="passMemb_confirm" id="passMemb_confirm">
-            <label><input type="checkbox" onclick="myFunction('passMemb_confirm')"><i>Afficher le mot de passe</i></label>
-        </div>
-
-        <div class="field">
-            <label for="accordMemb">J'accepte les <a href="<?= webSitePath('/cgu.php')?>">conditions générales d'utilisation</a></label>
-            <div class="controls">
-                <label class="font-h4"><input type="radio" name="accordMemb" value="on" /> Oui</label>
-                <label class="font-h4"><input type="radio" name="accordMemb" value="off" checked="checked" /> Non</label>
-            </div>
+            <input value="<?=$validator->oldField('passUser_confirm')?>" type="password" name="passUser_confirm" id="passUser_confirm">
+            <label><input type="checkbox" onclick="myFunction('passUser_confirm')"><i>Afficher le mot de passe</i></label>
         </div>
 
         
@@ -142,7 +132,7 @@ require_once __DIR__ . '/layouts/front/head.php';
     document.querySelectorAll(`input:not([type="file"], [type="submit"], [type="hidden"], [type="password"], [type="radio"])`).forEach(el=>el.value="lorem_ipsum_input");
     document.querySelectorAll(`input[type="datetime-local"]`).forEach(el=>el.value='2022-02-19T22:21');
     document.querySelectorAll(`input[type="password"]`).forEach(el=>el.value='qdqsd43&ds');
-    document.querySelectorAll(`input[name^="eMailMemb"]`).forEach(el=>el.value='qsdqd@gmail.com');
+    document.querySelectorAll(`input[name^="eMailUser"]`).forEach(el=>el.value='qsdqd@gmail.com');
 </script>
-<?php require_once __DIR__ . '/layouts/front/foot.php';?>
+<?php require_once __DIR__ . '/../layouts/front/foot.php';?>
 

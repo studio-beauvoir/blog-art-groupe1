7dbba4f890b6e524bcac0ad74f1605b02f474136
Arthaud Proust <arthaudp33@gmail.com>
2022-03-04 14:13:23 +0100

    improve mentions legales

--- mentions-legales.php
+++ mentions-legales.php
@@ -12,10 +12,12 @@ require_once __DIR__ . '/layouts/front/head.php';
     <br>
     <br>
     <h4>Hébergeur</h4>
+    <p>Le site est hébergé à l'iut Bordeaux Montaigne, rue Jacques Ellul.</p>
     <p>Le site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découler d’une nécessité de maintenance.</p>
     <br>
     <br>
     <h4>Cookies</h4>
-    <p>Des cookies de session, évitant aux utilisateurs de renseigner plusieurs fois les mêmes informations telles que les identifiants de connexion.</p>
+    <p>Des cookies sont déposés sur le navigateur lorsque vous créez un compte et lors de votre connexion. Ce sont des cookies de session, nécessaire au fonctionnement du site. Ce sont les seuls qui sont déposés.</p>
+    <p>Nous tenons à ce que la visite du site soit la plus fluide possible. C'est pour cela que nous n'utilisons pas de cookie optionnels visant à récolter vos données. Ceci nous autorise à ne pas afficher de bannière de consentement, les seuls cookies obligatoires étant strictements nécessaires au bon fonctionnement du site, et acceptés dans les conditions générales d'utilisation.</p>
 </div>
 <?php require_once __DIR__ . '/layouts/front/foot.php'; ?>
